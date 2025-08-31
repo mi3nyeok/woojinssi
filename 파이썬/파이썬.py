f2dd5b839a0663a33ab0768063551d28cf5367d4
mi3nyeok <mhmh081202@gmail.com>
2025-08-31 15:56:12 +0900

    first commit

---
+++ 파이썬/파이썬.py
@@ -0,0 +1,40 @@
+# 2. 사칙연산
+print(51+64*36-87)
+
+# 3. import
+import random
+print(random.random())
+
+# 4. 정의
+사과 = 2000
+바나나 = 2400
+
+print(사과*3+바나나*2)
+
+# 5 함수정의
+
+def ball():
+    print('basketball')
+
+ball()
+
+# 6. 함수정의(2)
+
+def number(num1, num2, num3):
+    print(num1+num2*num3)
+number(4, 5, 8)
+
+# 7. 함수정의(3)
+
+def woojin(num1, num2, num3):
+    return(num1-num2/num3)
+result = woojin(4, 5, 5)
+print(result)
+
+# 8. 글자수 세기
+print(len('최우진 농구 개잘함'))
+
+
+
+
+
